584ddf4c58f1e092fbe79dcbe194fdd473d9a835
jordi baliellas <jordi.bp@endeavour.nl>
2023-05-20 18:00:31 +0100

    Create reminder list component

--- src/app/domains/reminder/reminder.module.ts
+++ src/app/domains/reminder/reminder.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { remindersReducer, remindersName } from './state/reminders.reducer';
-
+import { ReminderListComponent } from './components/reminder-list/reminder-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ReminderListComponent
+  ],
   imports: [
     CommonModule,
     StoreModule.forFeature(remindersName, remindersReducer)
-  ]
+  ],
+  exports: [
+    ReminderListComponent
+  ],
 })
 export class ReminderModule { }
